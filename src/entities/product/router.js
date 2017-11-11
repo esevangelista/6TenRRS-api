52b378bf2cbe8798348a17b98bb238a5f691db6b
esevangelista <esevangelista1@up.edu.ph>
2017-11-12 04:13:45 +0800

    performs CRUD on product entity

---
+++ src/entities/product/router.js
@@ -0,0 +1,117 @@
+import { Router } from 'express';
+import * as Ctrl from './controller';
+
+const router = Router();
+
+router.get('/product/', async (req, res) => {
+  try {
+    const products = await Ctrl.getAllProduct();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully fetched products',
+      data: products
+    });
+  } catch (status) {
+    let message = '';
+    switch (status) {
+        case 404:
+          message = 'Products not found';
+          break;
+      case 500:
+        message = 'Internal server error';
+        break;
+    }
+    res.status(status).json({ status, message });
+  }
+});
+
+router.get('/product/:id', async (req, res) => {
+  try {
+    const product = await Ctrl.getProduct(req.params);
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully fetched product',
+      data: product
+    });
+  } catch (status) {
+    let message = '';
+    switch (status) {
+      case 404:
+        message = 'Product not found';
+        break;
+      case 500:
+        message = 'Internal server error';
+        break;
+    }
+    res.status(status).json({ status, message });
+  }
+});
+
+router.delete('/product/:id', async (req, res) => {
+  try {
+    await Ctrl.deleteProduct(req.params);
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully deleted product'
+    });
+  } catch (status) {
+    let message = '';
+    switch (status) {
+      case 404:
+        message = 'Product not found';
+        break;
+      case 500:
+        message = 'Internal server error';
+        break;
+    }
+    res.status(status).json({ status, message });
+  }
+});
+
+router.post('/product/', async (req, res) => {
+  try {
+    const id = await Ctrl.addProduct(req.body);
+    const product = await Ctrl.getProduct({ id });
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully added product',
+      data: product
+    });
+  } catch (status) {
+    let message = '';
+    switch (status) {
+      case 500:
+        message = 'Internal server error';
+        break;
+    }
+    res.status(status).json({ status, message });
+  }
+});
+
+router.put('/product/:id', async (req, res) => {
+  try {
+    await Ctrl.updateProduct(req.params, req.body);
+    const product = await Ctrl.getProduct(req.params);
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully updated product',
+      data: product
+    });
+  } catch (status) {
+    let message = '';
+    switch (status) {
+      case 404:
+        message = 'Product not found';
+        break;
+      case 500:
+        message = 'Internal server error';
+        break;
+    }
+    res.status(status).json({ status, message });
+  }
+});
+
+
+
+
+export default router;
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import customerRouter from './entities/customer/router';
 import rewardRouter from './entities/reward/router';
 import branchRouter from './entities/branch/router';
+import productRouter from './entities/product/router';
 //import session from 'express-session';
 
 
@@ -11,6 +12,8 @@ const router = Router();
 router.use(customerRouter);
 router.use(rewardRouter);
 router.use(branchRouter);
+router.use(productRouter);
+
 
 router.get('/', function(req, res) {
   res.json({message: ' access http://localhost:3001/api/<route>'});
