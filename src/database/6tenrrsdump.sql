5f4c67a407a14ef5fee2a5bf4cdef94989e9f058
esevangelista <esevangelista1@up.edu.ph>
2017-12-04 23:54:38 +0800

    updated

--- src/database/6tenrrsdump.sql
+++ src/database/6tenrrsdump.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `BRANCH`
 --
-
+use 6tenrrs;
 DROP TABLE IF EXISTS `BRANCH`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
